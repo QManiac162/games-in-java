b720ab948ae0c3877354f2a7a4ad3e1689b4b1cd
QManiac162 <quantummaniac609@gmail.com>
2023-06-23 18:54:26 +0530

    making a rectangle jump up with gravity

--- GIJ-1-code/src/userinterface/GameScreen.java
+++ GIJ-1-code/src/userinterface/GameScreen.java
@@ -1,15 +1,22 @@
 package userinterface;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
 
-public class GameScreen extends JPanel implements Runnable{
+public class GameScreen extends JPanel implements Runnable, KeyListener{
 	
-	private int i = 0;
+	public static final float GRAVITY = 0.1f;
+	private float x = 0;
+	private float y = 0;
+	private float speedY = 0;
 	private Thread thread;
 	
 	public GameScreen() {
-		// to color the game screen: setBackground(Color.RED);
 		thread = new Thread(this);
 	}
 	public void startGame() {
@@ -19,13 +26,44 @@ public class GameScreen extends JPanel implements Runnable{
 	@Override
 	public void run() {
 		while(true) {
-			System.out.println(i++);
 			try {
+				speedY+=GRAVITY;
+				y+=speedY;
+				repaint();
 				Thread.sleep(20);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		// to change the background after iterating each time through the loop otherwise more rectangles
+		// on the same background as the the before one
+		// method 1:
+		// super.paint(g);
+		// method 2:
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.BLACK);
+		g.drawRect((int)x, (int)y, 100, 100);
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		speedY = -4;
+		System.out.println("Key Presses!");
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Key Released!");
 	}
 }
